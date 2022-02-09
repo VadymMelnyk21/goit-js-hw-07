7858524ef0bfa8c677af4c0fe16b75e54521c342
VadymMelnyk21 <91023743+VadymMelnyk21@users.noreply.github.com>
2022-02-09 22:20:59 +0200

    рендер галереи

--- js/01-gallery.js
+++ js/01-gallery.js
@@ -2,3 +2,28 @@ import { galleryItems } from './gallery-items.js';
 // Change code below this line
 
 console.log(galleryItems);
+
+const galleryContainerRef = document.querySelector('.gallery');
+// console.log(galleryContainerRef)
+
+renderGallery(galleryItems);
+
+function renderGallery(arrayOfGallery) {
+    
+    const markup = arrayOfGallery
+        .map(({ preview, original, description }) => `
+            <div class="gallery__item">
+                <a class="gallery__link" href="${original}">
+                    <img
+                    class="gallery__image"
+                    src="${preview}"
+                    data-source="${original}"
+                    alt="${description}"
+                    />
+                </a>
+            </div>        
+        `)
+        .join(''); 
+    
+    galleryContainerRef.insertAdjacentHTML('beforeend', markup)
+}
