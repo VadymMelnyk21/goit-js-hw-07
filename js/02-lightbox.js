de3d160261de90fc1c6d51d54ab88e62efe1b9e7
VadymMelnyk21 <91023743+VadymMelnyk21@users.noreply.github.com>
2022-02-11 23:14:02 +0200

    создает галерею

--- js/02-lightbox.js
+++ js/02-lightbox.js
@@ -2,3 +2,23 @@ import { galleryItems } from './gallery-items.js';
 // Change code below this line
 
 console.log(galleryItems);
+
+const galleryContainerRef = document.querySelector('.gallery');
+
+galleryContainerRef.insertAdjacentHTML('beforeend', renderGallery(galleryItems));
+
+
+function renderGallery(arrayOfGallery) {
+    
+    const markup = arrayOfGallery
+        .map(({ preview, original, description }) => `
+            <li>
+                <a class="gallery__item" href="${original}">
+                    <img class="gallery__image" src="${preview}" alt="${description}" />
+                </a>
+            </li>      
+        `)
+        .join(''); 
+    
+    return markup;
+};
